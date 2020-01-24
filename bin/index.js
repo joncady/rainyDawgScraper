f6b0cd8df5c245ab912eb0289c410c70ccaf6d03
Jonathan Cady <joncady@Jonathans-MacBook-Pro.local>
2020-01-24 13:48:59 -0800

    removed extra stuff

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,16 @@
 #! /app/bin/node
 
-// establish connection to firebase
-
-// fetch of the site
-
 const axios = require('axios')
 const { firebase } = require('../config')
-
 
 const fetchStats = () => {
     axios.get('http://rainydawg.org:8000/stream.xspf')
         .then(function (response) {
-            // handle success
             let data = response.data;
             const CURRENT_STRING = "Current Listeners: ";
             let listenerIndex = data.indexOf(CURRENT_STRING) + CURRENT_STRING.length;
             let peakListenerIndex = data.indexOf("Peak Listeners: ");
             let parsedData = data.substring(listenerIndex, peakListenerIndex);
-            console.log(parsedData)
             firebase.database().ref("/").push({
                 listeners: Number(parsedData),
                 time: Date.now()
